2314ffcaba5b9bb3d256260f27b912af1b7938b9
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2019-03-25 23:17:40 +0000

    fraction

---
+++ metaprogramming/fraction.cpp
@@ -0,0 +1,33 @@
+#include <type_traits>
+#include <iostream>
+
+// 1. constexpr function ///////////////////
+// can't partially specialize function templates so at least I can't do better than this
+template <int N, int D> struct Fraction1 {};
+template <int S, int N, int D> constexpr auto scalar1() noexcept -> Fraction1<S*N, S*D> {};
+
+// 2. static class ///////////////////////
+template <int N, int D> struct Fraction2 { 
+    static constexpr int Num = N;
+    static constexpr int Den = D;
+};
+
+template <int S, typename F> 
+struct scalar2 { 
+    typedef Fraction2<S*F::Num, S*F::Den> result;
+};
+
+// 3. partially specialized class /////////
+// equivalent to the above
+template <int S, typename F> 
+struct scale {};
+
+template <int S, template<int, int> typename F, int N, int D> 
+struct scale<S, F<N, D>> {
+    typedef frak<S*F<N,D>::Num, S*F<N,D>::Den> result;
+};
+
+int main() {
+    static_assert(std::is_same<decltype(scalar1<2, 2, 2>()), Fraction1<4,4>>::value);
+    static_assert(std::is_same<scalar2<2, Fraction2<2,2>>::result, Fraction2<4,4>>::value);
+}
