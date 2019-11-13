e1148aee40d6677e8af7d1c0cd8f8cad8014a0f0
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2019-11-13 09:10:04 +0000

    memoize takes a function and stores the result locally

---
+++ metaprogramming/memoize.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+auto memoize(auto f) {
+    return [f](auto... args) {
+        using return_type = decltype(f(args...));
+        static return_type value{};
+        if (value == return_type{})   
+            value = f(args...);
+        return value;
+    };
+}
+
+int main() {
+    auto timesTwo = memoize([](int x){
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        return x * 2;
+    });
+    std::cout << timesTwo(2); // slow
+    std::cout << timesTwo(2); // instant
+}
