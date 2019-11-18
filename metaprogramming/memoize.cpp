0103a83c0ed9858944e2a9d0f3ce9ce3b7492b69
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2019-11-18 08:37:07 +0000

    clang-format

--- metaprogramming/memoize.cpp
+++ metaprogramming/memoize.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <thread>
 
-auto const memoize(auto f) {
+auto const memoize(auto const f) {
     return [f](auto... args) {
         using return_type = decltype(f(args...));
         static return_type value{};
-        if (value == return_type{})   
+        if (value == return_type{})
             value = f(args...);
         return value;
     };
 }
 
 int main() {
-    auto const timesTwo = memoize([](int x){
+    auto const timesTwo = memoize([](int x) {
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
         return x * 2;
     });
